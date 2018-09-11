e8c4bde7baabab9a88e503acd9aa2b64267278d6
qiuyu <530004000@qq.com>
2018-09-11 16:42:01 +0800

    lesson 2 https://laravel-china.org/docs/forum-in-laravel-tdd/2-test-topics/1634

--- app/Thread.php
+++ app/Thread.php
@@ -10,4 +10,12 @@ class Thread extends Model
      * @var array 所有属性均可更新, 后期修复.
      */
     protected $guarded = [];
+
+    /**
+     * @return string
+     */
+    public function path()
+    {
+        return '/threads/' . $this->id;
+    }
 }
---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function AUserCanViewAllThreads()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+    }
+
+    public function AUserCanReadASingleThread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
+        $response->assertSee($thread->title);
+    }
+}
