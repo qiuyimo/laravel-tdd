c93f04333c0d52927188a6c6dd4e638e7c9fbb87
qiuyu <530004000@qq.com>
2018-09-12 18:50:32 +0800

    lesson 18 https://laravel-china.org/docs/forum-in-laravel-tdd/18-pairs-of-reply-points/1650

--- app/Reply.php
+++ app/Reply.php
@@ -29,8 +29,28 @@ class Reply extends Model
      */
     protected $guarded = [];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    public function favorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        if (! $this->favorites()->where($attributes)->exists()) {
+            return $this->favorites()->create($attributes);
+        }
     }
 }
---
+++ tests/Feature/FavoritesTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class FavoritesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function an_authenticated_user_can_favorite_any_reply()
+    {
+        $this->signIn();
+
+        $reply = create('App\Reply');
+
+        // If I post a "favorite" endpoint
+        try{
+            $this->post('replies/' . $reply->id . '/favorites');
+            $this->post('replies/' . $reply->id . '/favorites');
+        }catch (\Exception $e){
+            $this->fail('Did not expect to insert the same record set twice.');
+        }
+
+
+        // It Should be record in the database
+        $this->assertCount(1, $reply->favorites);
+    }
+
+    /** @test */
+    public function guests_can_not_favorite_anything()
+    {
+        $this->post('/replies/1/favorites')->assertRedirect('/login');
+    }
+}
