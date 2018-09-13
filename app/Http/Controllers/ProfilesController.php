2390326b9972c85750f4fb50e229bca16a73a51b
qiuyu <530004000@qq.com>
2018-09-13 17:18:00 +0800

    lesson 26 https://laravel-china.org/docs/forum-in-laravel-tdd/26-display-action-flow/1658

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\User;
 
 class ProfilesController extends Controller
@@ -11,7 +10,18 @@ class ProfilesController extends Controller
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'threads' => $user->threads()->paginate(10)
+            'activities' => $this->getActivity($user)
         ]);
+    }
+
+    /**
+     * @param User $user
+     * @return static
+     */
+    protected function getActivity(User $user)
+    {
+        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
+            return $activity->created_at->format('Y-m-d');
+        });
     }
 }
