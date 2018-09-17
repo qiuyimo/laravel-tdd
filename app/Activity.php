ea23f1dff2b8c23de8ec4ff04b8507826ff1d37f
qiuyu <530004000@qq.com>
2018-09-17 18:31:49 +0800

    lesson 27 https://laravel-china.org/docs/forum-in-laravel-tdd/27-extract-the-query-from-the-controller-to-the-model/1659

--- app/Activity.php
+++ app/Activity.php
@@ -16,4 +16,21 @@ class Activity extends Model
     {
         return $this->morphTo();
     }
+
+    /**
+     * @param $user
+     * @param int $take
+     * @return mixed
+     */
+    public static function feed($user, $take = 50)
+    {
+        return Activity::query()->where('user_id', $user->id)
+            ->latest()
+            ->with('subject')
+            ->take($take)
+            ->get()
+            ->groupBy(function ($activity) {
+                return $activity->created_at->format('Y-m-d');
+            });
+    }
 }
